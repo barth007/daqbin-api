823e7f51e46fe0bddf0df6e680c491cc128d8d10
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2024-05-29 12:23:11 +0100

    done with file generation

--- daqbin/bin_api/urls.py
+++ daqbin/bin_api/urls.py
@@ -10,16 +10,3 @@ router.register(r'api/v1/bins', BinViewSet, basename='bins')
 router.register(r'api/v1/bin_data', BinDataViewSet, basename='bin_data')
 
 urlpatterns = router.urls
-
-# urlpatterns = [
-#     path('api/', UserAPI.as_view()),
-#     path('api/user/create/', UserAPI.as_view()),
-#     path('api/del-user/<str:pk>/', UserAPI.as_view()),
-#     path('api/user/<str:pk>/', UserDetailAPI.as_view()),
-#     path('api/bins/', BinAPI.as_view()),
-#     path('api/bin/create/', BinAPI.as_view()),
-#     path('api/bin/<str:pk>/', BinAPI.as_view()),
-#     path('api/sensordata/update/', BinDataAPI.as_view()),
-#     path('api/sensordata/', BinDataAPI.as_view()),
-#     path('api/sensordata<str:pk>/', BinParametersAPI.as_view()),
-# ]
